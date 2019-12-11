abe6dee58929398114d2216b04d327674982353e
Fernando <fernandols.1979@gmail.com>
2019-12-10 21:37:12 -0300

    refactory de portalEc

--- src/test/java/br/com/alelo/qa/web/page/AgendaPage.java
+++ src/test/java/br/com/alelo/qa/web/page/AgendaPage.java
@@ -1,10 +1,61 @@
 package br.com.alelo.qa.web.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import br.com.alelo.qa.web.support.PageObject;
 
-public class AgendaPage extends PageObject{
+public class AgendaPage extends PageObject {
+
+	@FindBy(id = "navbarDesktopAgenda")
+	protected WebElement menuAgenda;
+
+	@FindBy(id = "ScheDay_today")
+	protected WebElement elementoHoje;
+
+	@FindBy(id = "datetimmecustomDiv")
+	protected WebElement elementoData;
+	
+	@FindBy(id = "tableSummaryOperationListDesktop")
+	protected WebElement elementoResumo;
+	
+	@FindBy(id = "labelPerfilSelector")
+	protected WebElement elementoPerfil;
+	 
+	@FindBy(id = "scheduleTopBarMonthSelected")
+	protected WebElement elementoMes;
+	
+	@FindBy(id = "scheduleTopBarNextImage")
+	protected WebElement avancarMes;
+
+	@FindBy(id = "scheduleTopBarPreviousImage")
+	protected WebElement retrocederMes;
+	
+	@FindBy(id = "summary-more-details-button")
+	protected WebElement botaoVejaMaisDetalhes;
+	
+	@FindBy(id = "buttonComprovantes0")
+	protected WebElement botaoComprovante;
+
+	@FindBy(id = "summary-export-pdf-button-1")
+	protected WebElement pdfSuperiorAgenda;
+	
+	@FindBy(id = "summary-export-pdf-button-2")
+	protected WebElement pdfInferiorAgenda;
+	
+	@FindBy(id = "summary-export-csv-button-1")
+	protected WebElement csvSuperiorAgenda;
+	
+	@FindBy(id = "summary-export-csv-button-2")
+	protected WebElement csvInferiorAgenda;
+	
+	@FindBy(id = "summary-print-button-1")
+	protected WebElement ImprimirSuperiorAgenda;
+	
+	@FindBy(id = "summary-print-button-2")
+	protected WebElement ImprimirInferiorAgenda;
+	
 
 	public AgendaPage(WebDriver driver) {
 		super(driver);
--- src/test/java/br/com/alelo/qa/web/page/AntecipacaoPage.java
+++ src/test/java/br/com/alelo/qa/web/page/AntecipacaoPage.java
@@ -1,14 +1,77 @@
 package br.com.alelo.qa.web.page;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 
 import br.com.alelo.qa.web.support.PageObject;
 
 public class AntecipacaoPage extends PageObject{
+	
+	
+	@FindBy(id = "simulate-terms-check")
+	protected WebElement checkAceiteTermosRecebiveis;
+	
+	@FindBy(id = "anticipationButton")
+	protected WebElement btnAntecipar;
+	
+	@FindBy(xpath = "//*[@id=\"anticipationModalCallbackBody\"]/div[1]/h2[1]")
+	protected WebElement mensagemAntecipacao;
+	
+	@FindBy(id = "btnAnticipationChangeValue")
+	protected WebElement btnAlteraValor;
+	
+	@FindBy(id = "textAnticipationAntecipationValueMax")
+	protected WebElement valorMaximo;
+	
+	@FindBy(id = "formControlsText")
+	protected WebElement campoAlterarValor;
+	
+	@FindBy(id = "btnViewValueAntecipation")
+	protected WebElement btnVisualizarValores;
+	
+	@FindBy(id = "antecipation-opt-3-check")
+	protected WebElement opcao_1;
+	
+	@FindBy(id = "btnViewLiquidValueAntecipation")
+	protected WebElement celularValorLiquido;
+	
+	@FindBy(id = "btnAnticipationChange")
+	protected WebElement btnAlterarRecorrencia;
+	
+	@FindBy(id = "btnAnticipationModalAntecipationCancel")
+	protected WebElement botaoCancelarParcial;
+	
+	@FindBy(id = "btnAntecipationRecurrCancel")
+	protected WebElement botaoCancelarRecorrencia;
+	
+	@FindBy(id = "btnAnticipationCancel")
+	protected WebElement botaoCancelar;
+	
+	@FindBy(id = "anticipationModalAnticipationCloseIcon")
+	protected WebElement botaoFechar;
+	
+	@FindBy(id = "//*[@id=\"anticipationModalAnticipationDescriptionText\"]/div[2]/div/h5")
+	protected WebElement textoTermoAceite;
+	
+	@FindBy(id = "anticipationModalAnticipationTitle")
+	protected WebElement tituloModalAntecipacao;
+	
+	@FindBy(id = "btnAntecipationRecurrVisualize")
+	protected WebElement botaoDefinirRecebimento;
+	
+	@FindBy(id = "cardRecurr-DISABLED")
+	protected WebElement checkDesativar;
+	
+	@FindBy(id = "radioWeekly-SEG")
+	protected WebElement checkDiaSemanal;
+	
+	@FindBy(id = "cardRecurr-WEEKLY")
+	protected WebElement checkSemanal;
+		
 
 	public AntecipacaoPage(WebDriver driver) {
 		super(driver);
-		// TODO Auto-generated constructor stub
 	}
 
 }
