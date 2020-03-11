a058e9385766f462d174667afd9cea15c732d303
Fernando <fernandols.1979@gmail.com>
2020-03-11 16:07:05 -0300

    Testes automatizados de Recebimentos

--- src/test/java/br/com/alelo/qa/web/page/RecebimentosPage.java
+++ src/test/java/br/com/alelo/qa/web/page/RecebimentosPage.java
@@ -8,9 +8,8 @@ import org.openqa.selenium.support.FindBy;
 
 import br.com.alelo.qa.web.support.PageObject;
 
-public class RecebimentosPage extends PageObject{
+public class RecebimentosPage extends PageObject {
 
-	
 	@FindBy(id = "cnpj")
 	protected WebElement opcao_select;
 
@@ -26,77 +25,91 @@ public class RecebimentosPage extends PageObject{
 
 	@FindBy(id = "scheduleTopBarTotalValueToReceive")
 	protected WebElement valorAReceber;
-	
+
 	@FindBy(id = "scheduleTopBarNextImage")
 	protected WebElement avancarMes;
-	
+
 	@FindBy(css = ".totalValue")
 	protected List<WebElement> listAReceber;
+
+	@FindBy(xpath = "//*[@id='ScheDay_totalValue']/div[2]/span[2]/text()")
+	protected List<WebElement> listAReceber2;
 
 	@FindBy(id = "scheduleTopBarPreviousImage")
 	protected WebElement retrocederMes;
 
-	@FindBy(id = "scheduleTopBarMonthSelected")
+	@FindBy(id = "datetimmecustomDiv")
 	protected WebElement mesReferencia;
-	
+
+	@FindBy(xpath = "//*[@id='datetimmecustomDiv']/div/input")
+	protected WebElement mesExibido;
+
 	@FindBy(xpath = "//b[contains(.,'• 28.339.982/0001-60 - 2000496258')]")
 	protected WebElement ecRecebiveis_1;
-	
+
 	@FindBy(xpath = "//b[contains(.,'• 12.259.140/0001-68 - 1015000255')]")
 	protected WebElement ecRecebiveis_2;
-	
-	@FindBy(id = "imageLoader")
+
+	@FindBy(id = "loaderId")
 	protected WebElement loader;
-	
+
 	@FindBy(id = "ScheDay_today")
 	protected WebElement elementoHoje;
 
-	@FindBy(id = "receiveSummaryComboText")
+	@FindBy(id = "product")
 	protected WebElement campoVouReceber;
-	public String valorVouReceber= campoVouReceber.getText();
-	
+
 	@FindBy(id = "product")
 	protected WebElement comboProduto;
-	
+
 	@FindBy(xpath = "//input[@value='ALELO ALIMENTACAO']")
 	protected WebElement produtoAlimentacao;
-	
+
 	@FindBy(xpath = "//input[@value='ALELO REFEICAO']")
 	protected WebElement produtoRefeicao;
-	
+
 	@FindBy(xpath = "//input[@value='TODOS']")
 	protected WebElement produtoTodos;
-	
+
 	@FindBy(id = "tableSummaryOperationListDesktop")
 	protected WebElement tableListaDeOperacoes;
-	
+
 	@FindBy(id = "datetimmecustomDiv")
 	protected WebElement elementoData;
-	
+
 	@FindBy(id = "buttonComprovantes0")
 	protected WebElement botaoComprovante;
 
 	@FindBy(id = "summary-export-pdf-button-1")
 	protected WebElement pdfSuperiorAgenda;
-	
+
 	@FindBy(id = "summary-export-pdf-button-2")
 	protected WebElement pdfInferiorAgenda;
-	
+
 	@FindBy(id = "summary-export-csv-button-1")
 	protected WebElement csvSuperiorAgenda;
-	
+
 	@FindBy(id = "summary-export-csv-button-2")
 	protected WebElement csvInferiorAgenda;
-	
+
 	@FindBy(id = "summary-print-button-1")
 	protected WebElement ImprimirSuperiorAgenda;
-	
+
 	@FindBy(id = "summary-print-button-2")
 	protected WebElement ImprimirInferiorAgenda;
-	
+
 	@FindBy(xpath = "//input[@role='combobox']")
 	protected WebElement comboCnpj;
+
+	@FindBy(id = "ScheDay_today")
+	protected WebElement calendarVouReceberHoje;
+
+	@FindBy(id = "buttonComprovantes0")
+	protected WebElement btnComprovantes;
 	
+	@FindBy(xpath = "//*[@id='tableSummaryOperationListDesktop']/tbody")
+	protected WebElement tableDetalhe;
+
 	public RecebimentosPage(WebDriver driver) {
 		super(driver);
 		// TODO Auto-generated constructor stub
--- src/test/resources/features/Recebimentos.feature
+++ src/test/resources/features/Recebimentos.feature
@@ -4,14 +4,13 @@
 # * Change: 08/01/2019 | Author: Jessé Dantas - Taking
 # * Caminho: Portal Estabelecimento Comercial
 # */
-
 Funcionalidade: Recebimentos
   Permite a consulta dos valores a serem disponibilizados ao usuario ao longo do mes
 
   Contexto: 
-  Dado que estou na logado no portal EC "363.981.960-82", "Alelo2020@"
-  
-  @RecebimentoMes
+    Dado que estou na logado no portal EC "273.824.010-06", "Alelo2020@"
+
+  @Recebimentoss
   Esquema do Cenario: <VSTS> - Recebimento mensal <cenario>
     Quando eu clicar no menu recebimentos "<cnpj>"
     Entao devo validar que é exibido o valor recebido e o valor a receber do meu ec "<periodo>"
@@ -22,14 +21,27 @@ Funcionalidade: Recebimentos
       | 9999 | Realizar consulta de valores a receber no proximo mes  | mês seguinte | 28339982000160 |
       | 9999 | Realizar consulta de valores recebidos no mes anterior | mês anterior | 28339982000160 |
 
-  @RecebimentoMes @produto1
+  @Recebimentos1 @RecebimentoPorProduto
   Esquema do Cenario: <VSTS> - Recebimento detalhado <cenario>
     Quando eu clicar no menu recebimentos "<cnpj>"
     E selecionar a data atual e a opcao de "<produto>"
     Entao devo visualizar a lista de valores a receber de acordo com o produto selecionado "<produto>"
 
     Exemplos: 
-      | VSTS | cenario                                                  | produto     | cnpj           |
-      | 9999 | Consultar Recebimentos por tipo de produto - REFEIÇÃO    | REFEIÇÃO    | 28339982000160 |
-      #| 9999 | Consultar Recebimentos por tipo de produto - ALIMENTAÇÃO | ALIMENTAÇÃO | 28339982000160 |
-      #| 9999 | Consultar Recebimentos por tipo de produto - TODOS       | TODOS       | 28339982000160 |
+      | VSTS | cenario                                                  | produto           | cnpj           |
+      | 9999 | Consultar Recebimentos por tipo de produto - REFEIÇÃO    | ALELO REFEICAO    | 28339982000160 |
+      | 9999 | Consultar Recebimentos por tipo de produto - ALIMENTAÇÃO | ALELO ALIMENTACAO | 28339982000160 |
+      | 9999 | Consultar Recebimentos por tipo de produto - TODOS       | TODOS             | 28339982000160 |
+
+  @Recebimentos @FERNANDO
+  Esquema do Cenario: <VSTS> - Recebimento detalhado <cenario>
+    Quando eu clicar no menu recebimentos "<cnpj>"
+    E selecionar a data atual e a opcao de "<produto>"
+    E devo visualizar a lista de valores a receber de acordo com o produto selecionado "<produto>"
+    Entao devo validar que foi apresentado uma tabela com os detalhes das transacoes de acordo com o filtro "<produto>"
+
+    Exemplos: 
+      | VSTS | cenario                                                                | produto           | cnpj           |
+      | 9999 | Consultar detalhe das transações de acordo com o produto - REFEIÇÃO    | ALELO REFEICAO    | 28339982000160 |
+      | 9999 | Consultar detalhe das transações de acordo com o produto - ALIMENTAÇÃO | ALELO ALIMENTACAO | 28339982000160 |
+      | 9999 | Consultar detalhe das transações de acordo com o produto- TODOS        | TODOS             | 28339982000160 |
