af6476d3624dc30f7f2e5a51626beb51104214d5
Fernando <fernandols.1979@gmail.com>
2020-04-22 10:33:55 -0300

    merge UAT

--- src/test/java/br/com/alelo/qa/CucumberRunnerTest.java
+++ src/test/java/br/com/alelo/qa/CucumberRunnerTest.java
@@ -26,7 +26,7 @@ import java.util.Date;
 @RunWith(Cucumber.class)
 @CucumberOptions(strict = false, features = {"src/test/resources/features/desenvolve/","src/test/resources/features/ec/", "src/test/resources/features/operador/"}, plugin = {
         "json:target/cluecumber-report/cucumber.json", "junit:target/junit.xml"}, glue = {
-        "classpath:br.com.alelo.qa.features.steps"}, tags = {"@SimulacaoCancelamentoWeb"})
+        "classpath:br.com.alelo.qa.features.steps"}, tags = {"@Regressivo"})
 
 public class CucumberRunnerTest {
 
